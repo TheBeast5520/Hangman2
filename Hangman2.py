8ab88f48f9af1eb3e334e19fe2f21f2ccb27632b
TheBeast5520 <66189677+TheBeast5520@users.noreply.github.com>
2020-08-04 21:06:08 -0700

    Update Hangman2.py

--- Hangman2.py
+++ Hangman2.py
@@ -1,9 +1,30 @@
 
 class Hangman:
 
-	def __init__(self):
+    def __init__(self):
+        
+        self.numPlayers = int(input("Enter the number of players (1/2): \n"))
 
-		print("This is a hangman object")
+        if (self.numPlayers==1):
+            self.oneP()
+        elif (self.numPlayers==2):
+            self.twoP()
+        else:
+            print("Please enter a valid number.\n")
 
-while (input("Do you want to play again?\n").strip().lower()=="yes"): 
-	game = Hangman()
+    def oneP(X):
+        print("oneP")
+
+    def twoP(self):
+        print("twoP")
+
+
+while (True): 
+    yn = input("Do you want to play again?\n").strip().lower()
+    if not yn=="yes":
+        break
+    game = Hangman()
+
+# draw_hangman feature
+# one player_game
+# two play_game
