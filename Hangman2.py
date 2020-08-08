2e512fd24111d390c1352eac92744658179d896d
TheBeast5520 <email@example.com>
2020-08-07 18:34:33 -0700

    Update Hangman2.py

--- Hangman2.py
+++ Hangman2.py
@@ -1,42 +1,44 @@
-import random
-import turtle
+# import random
+# import turtle
 
-class Hangman:
+# class Hangman:
 
-    fin = open("wordlist.txt","r")
-    words = [i.strip() for i in fin.readlines()]
+#     fin = open("wordlist.txt","r")
+#     words = [i.strip() for i in fin.readlines()]
 
-    def __init__(self):
+#     def __init__(self):
         
-        self.numPlayers = int(input("Enter the number of players (1/2): \n"))
+#         self.numPlayers = int(input("Enter the number of players (1/2): \n"))
 
-        if (self.numPlayers==1):
-            self.oneP()
-        elif (self.numPlayers==2):
-            self.twoP()
-        else:
-            print("Please enter a valid number.\n")
+#         if (self.numPlayers==1):
+#             self.oneP()
+#         elif (self.numPlayers==2):
+#             self.twoP()
+#         else:
+#             print("Please enter a valid number.\n")
 
-    def oneP(self):
-        print("You will now play against the computer.")
-        self.customWords = []
-        yn = input("Would you like to make your own custom words? (y/n)\n").strip().lower()
-        if (yn=="y"):
-            self.cWord=random.choice(self.words) #WIP
-        else:
-            self.cWord = random.choice(self.words)
-        print(self.cWord)
+#     def oneP(self):
+#         print("You will now play against the computer.")
+#         self.customWords = []
+#         yn = input("Would you like to make your own custom words? (y/n)\n").strip().lower()
+#         if (yn=="y"):
+#             self.cWord=random.choice(self.words) #WIP
+#         else:
+#             self.cWord = random.choice(self.words)
+#         print(self.cWord)
 
-    def twoP(self):
-        print("twoP")
+#     def twoP(self):
+#         print("twoP")
 
 
-while (True): 
-    game = Hangman()
-    yn = input("Do you want to play again? (y/n)\n").strip().lower()
-    if not yn=="y":
-        break
+# while (True): 
+#     game = Hangman()
+#     yn = input("Do you want to play again? (y/n)\n").strip().lower()
+#     if not yn=="y":
+#         break
 
-# draw_hangman feature
-# one player_game
-# two play_game
+# # draw_hangman feature
+# # one player_game
+# # two play_game
+
+from tkinter import *
