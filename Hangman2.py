b31f70f701d0eae434ce734f077d3eac89c5725d
TheBeast5520 <email@example.com>
2020-08-07 18:47:08 -0700

    Update Hangman2.py

--- Hangman2.py
+++ Hangman2.py
@@ -42,3 +42,57 @@
 # # two play_game
 
 from tkinter import *
+from PIL import ImageTk, Image
+
+class Graphics(Canvas):
+
+    pics = {
+        '1':'backgrounds/step1',
+        '2':'backgrounds/step2',
+        '3':'backgrounds/step3',
+        '4':'backgrounds/step4',
+        '5':'backgrounds/step5',
+        '6':'backgrounds/step6'
+    }
+
+    def __init__(self, master):
+
+        self.width=500
+        self.height=550
+
+        Canvas.__init__(self, master, bg='saddle brown', width=self.width, height=self.height, highlightthickness=0)
+
+        self.createImage('1')
+
+    def createImage(self,picName,size=()):
+        if size==():
+            size=(self.width,self.height)
+        filename = self.pics[picName]
+        self.im = Image.open(filename)
+        self.resizePic(size)
+        self.img=ImageTk.PhotoImage(self.im)
+        self.pic = self.create_image(self.slength/2,self.slength/2,anchor=CENTER,\
+                                     image=self.img)
+
+    def resizePic(self,size):
+        self.im = self.im.resize(size,Image.ANTIALIAS)
+
+class Hangman(Frame):
+
+    def __init__(self, master):
+
+        Frame.__init__(self, master)
+        self.grid()
+
+        self.graphics = Graphics(self)
+        self.graphics.grid(row=0,column=0)
+
+
+
+def play_hangman():
+    root = Tk()
+    root.title='Hangman'
+    game = Hangman(root)
+    root.mainloop()
+
+play_hangman()
